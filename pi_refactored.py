0d5e2c939d7c926ca9b76538304530189c9602c8
Colin Sauze <cos@aber.ac.uk>
2019-03-28 17:56:26 +0000

    refactored version with separate program illustrating modules

--- pi_refactored.py
+++ pi_refactored.py
@@ -9,7 +9,6 @@ pi/4 (as we only use a quarter circle) multiply this by 4 and we'll get an
 approximation for pi increasing the number of points increases the accuracy to
 a point, but gives diminishing returns over 10000 points
 """
-import sys
 import numpy as np
 
 # pylint: disable=no-member
@@ -18,14 +17,16 @@ import numpy as np
 np.random.seed(2017)
 
 
-def generate_points():
+def generate_points(num_points):
     '''
-    generates a set of random points, tests which are inside a circle
+    Generates a set of random points, tests which are inside a circle.
+    Takes number of points to generate as a parameter, returns the number
+    inside the circle.
     '''
-    x_coords = np.float32(np.random.uniform(size=10000))
-    y_coords = np.float32(np.random.uniform(size=10000))
+    x_coords = np.float32(np.random.uniform(size=num_points))
+    y_coords = np.float32(np.random.uniform(size=num_points))
 
-    radii = np.sqrt((x_coords**2)+(y_coords*y_coords))
+    radii = np.sqrt((x_coords**2)+(y_coords**2))
 
     # count how many points are in the circle,
     # they will be in the circle if x^2 + y^2 is less than 1.0
@@ -35,17 +36,17 @@ def generate_points():
     return inside_circle_count
 
 
-def calculate_pi(numpoints):
+def calculate_pi(num_points):
     '''
-    calculates pi
+    Calculates pi, takes the number of points to calculate with as a parameter.
+    Returns the value of pi which was calculated.
     '''
-    inside_circle_count = generate_points()
+    inside_circle_count = generate_points(num_points)
     # get how many points were found inside the circle
-
-    print("pi is %f" % (4.0 * (inside_circle_count / float(10000))),
-          "from ", numpoints, " samples")
+    return (4.0 * (inside_circle_count / float(num_points)))
 
 
-calculate_pi(10000)
-
-sys.exit(1)
+if __name__ == '__main__':
+    n_samples = 10000
+    pi = calculate_pi(n_samples)
+    print("pi is %f from %d samples" % (pi, n_samples))
---
+++ pi_refactored_run.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Mar 28 14:49:04 2019
+
+@author: colin
+"""
+
+import pi_refactored
+
+pi_refactored.calculate_pi(10000)
